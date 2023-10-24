d75e2a4679e6de8570e0aaf1fbac89aca8e185a9
beatricenjerimwangi <bbeatricemwangi@gmail.com>
2023-10-24 16:29:06 -0400

    i have added my first fetch method to fetch books

--- book-review/JS/script.js
+++ book-review/JS/script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     let bookList = ddocument.getElementById("book-list");
+    fetch('https://wolnelektury.pl/api/books/');
+        //.then(res=>res.json())
 
-    
-}
+
+})
